df59f6fcf886770a8a10044886ad145f75d2085e
David Nguyen <tnguyen.david@gmail.com>
2019-11-06 15:07:11 -0800

    Added backdrop for modal tutorial

--- src/App/index.js
+++ src/App/index.js
@@ -9,6 +9,24 @@ class App extends React.Component {
       <React.Fragment>
         {/* <TutorialModal /> */}
         <Header />
+        <div style={{ textAlign: 'center', marginTop: '100px'}}>
+          <svg style={{ height: '500px'}}>
+            <g transform="translate(0,150)">
+              <rect style={{ width: '50px', height: '15px', fill: 'yellow'}}/>
+              <text x='22.5' y='35' style={{ textAnchor: 'middle' }}>2</text>
+            </g>
+
+            <g transform="translate(55)">
+              <rect style={{ width: '50px', height: '165px', fill: 'yellow'}}/>
+              <text x='22.5' y='185' style={{ textAnchor: 'middle' }}>22</text>
+            </g>
+
+            <g transform="translate(110, 90)">
+              <rect style={{ width: '50px', height: '75px', fill: 'yellow'}}/>
+              <text x='22.5' y='95' style={{ textAnchor: 'middle' }}>10</text>
+            </g>
+          </svg>
+        </div>
       </React.Fragment>
     )
   }
--- src/TutorialModal/index.js
+++ src/TutorialModal/index.js
@@ -5,19 +5,21 @@ export default class TutorialModal extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
-      <div className="modalWrapper">
-        <div className="pageCounter">1/4</div>
-        <h2>Welcome to Sort Visualizer</h2>
-        <h6>This tutorial will show you how to navigate and use all the features of this application.</h6>
-        <p>Feel free to click "Skip Tutorial" if you want to head straight to the visualizer.</p>
-        <footer className="modalFooter">
-          <button>Skip Tutorial</button>
-          <button>Previous</button>
-          <button>Next</button>
-        </footer>
+      <div className="modalBackdrop">
+        <div className="modalWrapper">
+          <div className="pageCounter">1/4</div>
+          <h2>Welcome to Sort Visualizer</h2>
+          <h6>This tutorial will show you how to navigate and use all the features of this application.</h6>
+          <p>Feel free to click "Skip Tutorial" if you want to head straight to the visualizer.</p>
+          <footer className="modalFooter">
+            <button>Skip Tutorial</button>
+            <button>Previous</button>
+            <button>Next</button>
+          </footer>
+        </div>
       </div>
     )
   }
